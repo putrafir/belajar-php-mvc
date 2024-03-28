a8a7cc6587c1f88231b1e0c3885173d25285ad3a
putrafir <141028423+putrafir@users.noreply.github.com>
2024-03-28 21:58:01 +0700

    commit

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,5 +1,12 @@
 <div class="container mt-3">
-    <div class="col-6">
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash() ?>
+        </div>
+    </div>
+
+    <div class="col-lg-6">
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
             Tambah Data Mahasiswa
         </button>
